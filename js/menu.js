98c70d698b6f2f150489ccbc87aa814ac90d1500
Helena Avis <helenaavis48@gmail.com>
2025-02-28 14:36:31 +0000

    Fixed issue where nav menu would still be hidden after resizing from mobile to desktop.

--- js/menu.js
+++ js/menu.js
@@ -8,20 +8,42 @@ class Menu {
 
     init() {
         this.buttonImage.addEventListener('click', (e) => this.toggleMenu())
+        window.addEventListener('resize', (e) => {
+            if (window.innerWidth > 800) {
+                console.log('Window width over 800px - showing nav')
+                this.openMenu()
+            }
+        })
+
+        if (window.innerWidth > 800) {
+            this.nav.classList.add('header__nav--visible')
+        } else {
+            this.nav.classList.add('header__nav--hidden')
+        }
     }
 
     toggleMenu() {
         if (window.innerWidth <= 800) {
-            if (this.nav.style.display === 'none') {
-                console.log('Opening menu')
-                this.buttonText.textContent = 'CLOSE'
-                this.nav.style.display = 'flex'
+            if (this.nav.classList.contains('header__nav--hidden')) {
+                this.openMenu()
             } else {
-                console.log('Closing menu')
-                this.buttonText.textContent = 'MENU'
-                this.nav.style.display = 'none'
+                this.closeMenu()
             }
         }
+    }
+
+    openMenu() {
+        console.log('Opening menu')
+        this.buttonText.textContent = 'CLOSE'
+        this.nav.classList.add('header__nav--visible')
+        this.nav.classList.remove('header__nav--hidden')
+    }
+
+    closeMenu() {
+        console.log('Closing menu')
+        this.buttonText.textContent = 'MENU'
+        this.nav.classList.add('header__nav--hidden')
+        this.nav.classList.remove('header__nav--visible')
     }
 }
 
